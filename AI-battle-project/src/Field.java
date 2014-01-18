c0f116a0cd58bf1cbc9b7b514a70f563f288ecb7
S7-392-9890 <S7-392-9890@10.255.152.32>
2014-01-18 04:58:49 -0500

    This adds the field background

--- AI-battle-project/src/Field.java
+++ AI-battle-project/src/Field.java
@@ -1,9 +1,34 @@
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-class Field
+public class Field
 {
+	Sprite sprite;
+	Texture texture;
+	float x,y;
+	
+	public Field()
+	{
+		texture = new Texture(Gdx.files.internal("assets/grass.png"));
+		sprite = new Sprite(texture,20,20);
+	}
     public void draw(SpriteBatch batch)
     {
-        //draw here
+    	x = 0;
+    	y = 0;
+    	
+    	while(y < 480)
+    	{
+    		x = 0;
+    		while(x < 800)
+    		{
+    			sprite.setPosition(x, y);
+        		sprite.draw(batch);
+        		x = x + sprite.getWidth();        		
+    		}
+    		y = y + sprite.getHeight();
+    	}
     }
 }
