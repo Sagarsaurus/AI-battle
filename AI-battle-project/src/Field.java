974ead4a05490383cfde49d721905c427d1cde19
S7-392-9890 <S7-392-9890@10.255.152.32>
2014-01-18 07:00:35 -0500

    kjg

--- AI-battle-project/src/Field.java
+++ AI-battle-project/src/Field.java
@@ -3,32 +3,56 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.util.Random;
+
 public class Field
 {
-	Sprite sprite;
-	Texture texture;
+	Sprite fieldSprite;
+	Sprite[] rockSpirtes;
+	Texture fieldTexture, rockTexture;
 	float x,y;
+	int rockX, rockY, numRock;
+	Random rand;
 	
 	public Field()
 	{
-		texture = new Texture(Gdx.files.internal("assets/grass.png"));
-		sprite = new Sprite(texture, AIBattle.CHARACTER_SIZE, AIBattle.CHARACTER_SIZE);
+		fieldTexture = new Texture(Gdx.files.internal("assets/grass.png"));
+		fieldSprite = new Sprite(fieldTexture, AIBattle.CHARACTER_SIZE, AIBattle.CHARACTER_SIZE);
+		
+		rockTexture = new Texture(Gdx.files.internal("assets/stone.png"));
+
+		rand = new Random();
+
+		numRock= 20;
+
+		rockSpirtes = new Sprite[numRock];
+				
+		for(int i = 0; i < numRock; i++)
+		{
+			rockSpirtes[i] = new Sprite(rockTexture, AIBattle.CHARACTER_SIZE, AIBattle.CHARACTER_SIZE);
+			rockSpirtes[i].setPosition(AIBattle.CHARACTER_SIZE*2 + rand.nextInt(800 - AIBattle.CHARACTER_SIZE*4),rand.nextInt(480-AIBattle.CHARACTER_SIZE));
+		}
 	}
     public void draw(SpriteBatch batch)
     {
     	x = 0;
     	y = 0;
     	
+    	//Draws the field
     	while(y < 480)
     	{
     		x = 0;
     		while(x < 800)
     		{
-    			sprite.setPosition(x, y);
-        		sprite.draw(batch);
-        		x = x + sprite.getWidth();        		
+    			fieldSprite.setPosition(x, y);
+    			fieldSprite.draw(batch);
+        		x = x + fieldSprite.getWidth();        		
     		}
-    		y = y + sprite.getHeight();
+    		y = y + fieldSprite.getHeight();
+    	}
+    	for(int i = 0; i < numRock; i ++)
+    	{
+    		rockSpirtes[i].draw(batch);
     	}
     }
 }
