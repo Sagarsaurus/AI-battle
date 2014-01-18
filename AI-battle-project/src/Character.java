9b84d84a42b3ed333f9eebd46dd679bed67654b2
Karan Pahawa <kpahawa@gmail.com>
2014-01-18 02:02:40 -0500

    Basic character class updated

--- AI-battle-project/src/Character.java
+++ AI-battle-project/src/Character.java
@@ -1,4 +1,25 @@
+import java.awt.Graphics;
+import java.util.Random;
 
-public class Character {
-
+public class Character{
+	protected int x;
+	protected int y;
+	int yChange;
+	boolean LIFE = true;
+	
+	public Character(){
+		Random randX = new Random();
+		Random randY = new Random();
+		x = randX.nextInt(500);
+		y = randY.nextInt(500);
+	}
+	
+	public Character(int newX, int newY){
+		x=newX;
+		y=newY;
+	}
+	
+	public void draw(Graphics g){
+		
+	}
 }
